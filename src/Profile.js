53ebbad059ffdf17fca28c5a382c5ce1a95b42ee
kcbobbe <kcbobbe@gmail.com>
2018-06-11 08:55:01 -0400

    worked on profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './App.css'
 import './DashboardLoggedIn.js'
+import Question from './Question'
 // import Login from './Login'
 import 'shoelace-css/dist/shoelace.css'
 import request from 'superagent'
@@ -11,7 +12,7 @@ class Profile extends Component {
     this.state = {
       username: '',
       email: '',
-      userQuestions: '?????????',
+      userQuestions: [],
       userAnswers: 'Answers'
     }
 
@@ -20,15 +21,18 @@ class Profile extends Component {
 
   componentDidMount () {
     request
-      .get(`https://whispering-stream-62515.herokuapp.com/api/v1/users/${localStorage.id}`)
+      // .get(`https://whispering-stream-62515.herokuapp.com/api/v1/users/${localStorage.id}`)
+      .get(`https://whispering-stream-62515.herokuapp.com/api/v1/users/4`)
       .set('X-Requested-With', 'XMLHttpRequest')
       .then(res => {
         console.log(res)
         this.setState({
           username: res.body.data.attributes.username,
           email: res.body.data.attributes.email_address,
-          userQuestions: res.body.data.attributes.questions,
-          userAnswers: res.body.data.attributes.answers
+          userQuestions: res.body.data.attributes.questions.questions,
+          userAnswers: res.body.data.attributes.answers,
+          questionNumber: res.body.data.attributes.questions.total_questions,
+          answerNumber: res.body.data.attributes.answers.total_answers
         })
       })
   }
@@ -41,11 +45,23 @@ class Profile extends Component {
   render () {
     return (
       <div className='Profile'>
-        <h1>PROFILE</h1>
+        <h1>Your Proflie</h1>
+        <div>Username: {this.state.username}</div>
+        <div>Email Address: {this.state.email}</div>
+        <div>Number of questions asked: {this.state.questionNumber}</div>
+        <div>Number of questions answered: {this.state.answerNumber}</div>
+        <div className='questions-column'>
+          <h2>Questions you've asked!</h2>
+          <div className='questions-container'>
+            {this.state.userQuestions.map((question, idx) => (
+              <div key={idx}>
+                <Question question={question} />
+              </div>
+            ))}
+          </div>
+        </div>
         <button className='button-danger' onClick={this.props.profileState}>Cancel</button>
         <button onClick={this.logout}>Logout</button>
-        <div>{this.state.username}</div>
-        <div>{this.state.email}</div>
         {/* <div>{this.state.userQuestions[0]}</div>
         <div>{this.state.userAnswers[0]}</div> */}
       </div>
